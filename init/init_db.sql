d45bae772d43fe8c029a81d2600bf7df06501650
Nguyễn Xuân Nam <namnx@rabiloo.com.vn>
2021-06-20 15:38:02 +0700

    update docker-compose.yml

--- init/init_db.sql
+++ init/init_db.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `product_ms`;
 CREATE DATABASE IF NOT EXISTS `cart_ms`;
 
-# create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+# # create root user and grant rights
+# CREATE USER 'root'@'localhost' IDENTIFIED BY '123456';
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
--- product-service/src/main/resources/data.sql
+++ product-service/src/main/resources/data.sql
@@ -1,8 +1,24 @@
+CREATE TABLE IF NOT EXISTS product
+(
+    `id`           INT         NOT NULL,
+    `code` VARCHAR(45) NOT NULL,
+    `name`         VARCHAR(45) NOT NULL,
+    `description`  VARCHAR(45) NULL,
+    `price`        DOUBLE      NOT NULL,
+    `quantity`     INT(11)     NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `code_UNIQUE` (`code` ASC)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_bin;
+
+
 DELETE
 FROM product;
 
 insert into product(id, code, name, description, price, quantity)
-VALUES (1, 'P001', 'NOKIA 1', 'NOKIA 1 description',250, 25),
-       (2, 'P002', 'NOKIA 2', 'NOKIA 2 description',230, 32),
-       (3, 'P003', 'NOKIA 3', 'NOKIA 3 description',210, 50)
+VALUES (1, 'P001', 'NOKIA 1', 'NOKIA 1 description', 250, 25),
+       (2, 'P002', 'NOKIA 2', 'NOKIA 2 description', 230, 32),
+       (3, 'P003', 'NOKIA 3', 'NOKIA 3 description', 210, 50)
 ;
